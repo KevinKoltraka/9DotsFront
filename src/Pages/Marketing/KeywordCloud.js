f56b5106dc688dd9bc198e0c51318ea60768a5a9
Kevin Koltraka <kevikoltraka157@gmail.com>
2025-02-14 21:32:59 +0100

    changes

--- src/Pages/Marketing/KeywordCloud.js
+++ src/Pages/Marketing/KeywordCloud.js
@@ -1,5 +1,6 @@
 import React from "react";
-import WordCloud from "react-wordcloud";
+import { Wordcloud } from "@visx/wordcloud";
+import { scaleLog } from "d3-scale";
 import "./main.scss";
 
 const words = [
@@ -10,13 +11,26 @@ const words = [
   { text: "Unreliable", value: 15 },
 ];
 
-const options = { rotations: 2, rotationAngles: [0, 90], fontSizes: [10, 50] };
+const fontScale = scaleLog()
+  .domain([Math.min(...words.map((w) => w.value)), Math.max(...words.map((w) => w.value))])
+  .range([10, 50]); // Font size range
 
 const KeywordCloud = () => {
   return (
     <div className="word-cloud">
       <h3>Trending Keywords</h3>
-      <WordCloud words={words} options={options} />
+      <svg width={400} height={300}>
+        <Wordcloud
+          words={words}
+          width={400}
+          height={300}
+          fontSize={(word) => fontScale(word.value)}
+          rotate={() => (Math.random() > 0.5 ? 0 : 90)}
+          font={"Arial"}
+          padding={5}
+          fill={(word) => (word.value > 30 ? "#007BFF" : "#FF5733")} // Customize colors
+        />
+      </svg>
     </div>
   );
 };
